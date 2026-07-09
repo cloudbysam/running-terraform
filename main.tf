a4ea7a19ea1c1917223ac543b6a700b0529308c9
Samuel Ajala <samuelaj220@gmail.com>
2026-07-09 16:05:14 +0100

    feat: create security group to allow inbound traffic to the server.

--- main.tf
+++ main.tf
@@ -3,22 +3,36 @@ provider "aws" {
 }
 
 resource "aws_instance" "main_server" {
-    ami = "ami-002192a70217ac181"
+    ami = "ami-0b6d9d3d33ba97d99"
     instance_type = "t2.micro"
+    key_name = "N.Virginia-key"
+    vpc_security_group_ids = [ aws_security_group.network.id ]
 
     user_data_base64 = base64encode(<<-EOF
-    #!/bin/bash
-    sudo apt update -y
-    sudo apt install -y busybox
-        mkdir -p /var/www
-        echo "Hello, World from Terraform." > /var/www/index.html
-        nohup busybox httpd -f -p 8080 -h /var/www &
-        EOF
-        )
+#!/bin/bash
+sudo apt update -y
+sudo apt install -y busybox
+mkdir -p /var/www
+echo "Hello, World from Terraform :( ." > /var/www/index.html
+nohup busybox httpd -f -p 8080 -h /var/www &
+EOF
+    )
     
     user_data_replace_on_change = true
 
     tags = {
       Name = "main-server"
     }
+}
+
+resource "aws_security_group" "network" {
+  name = "inbound-network"
+
+  ingress  {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
 }
