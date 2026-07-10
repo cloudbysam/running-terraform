01d93f7a25bd93ef8bc4eac354b81b21cf4d84cc
Samuel Ajala <samuelaj220@gmail.com>
2026-07-10 19:46:38 +0100

    feat: add autoscaling group resource

--- main.tf
+++ main.tf
@@ -2,13 +2,16 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "main_server" {
-    ami = "ami-0b6d9d3d33ba97d99"
+resource "aws_launch_template" "main_server" {
+    image_id = "ami-0b6d9d3d33ba97d99"
     instance_type = "t2.micro"
-    key_name = "N.Virginia-key"
-    vpc_security_group_ids = [ aws_security_group.network.id ]
 
-    user_data_base64 = base64encode(<<-EOF
+    network_interfaces {
+      security_groups =  [ aws_security_group.network.id ]
+      associate_public_ip_address = true
+    }
+
+    user_data = base64encode(<<-EOF
 #!/bin/bash
 sudo apt update -y
 sudo apt install -y busybox
@@ -17,12 +20,6 @@ echo "Hello, World from Terraform :) ." > /var/www/index.html
 nohup busybox httpd -f -p ${var.server_port} -h /var/www &
 EOF
     )
-    
-    user_data_replace_on_change = true
-
-    tags = {
-      Name = "main-server"
-    }
 }
 
 resource "aws_security_group" "network" {
@@ -35,6 +32,17 @@ resource "aws_security_group" "network" {
     cidr_blocks = [ "0.0.0.0/0" ]
   }
 
+}
+
+resource "aws_autoscaling_group" "auto-scale" {
+  desired_capacity   = 2
+  max_size           = 4
+  min_size           = 2
+
+  launch_template {
+    id      =  aws_launch_template.main_server.id
+    version = "$Latest"
+  }
 }
 
 variable "server_port" {
