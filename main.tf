33620f3f169126b1f7cfc4c36872b86966b7109d
Samuel Ajala <samuelaj220@gmail.com>
2026-07-10 20:18:21 +0100

    feat: use VPC subnet data sources for ASG

--- main.tf
+++ main.tf
@@ -20,6 +20,11 @@ echo "Hello, World from Terraform :) ." > /var/www/index.html
 nohup busybox httpd -f -p ${var.server_port} -h /var/www &
 EOF
     )
+
+# Required when using a launch configuration with an auto scaling group.
+lifecycle {
+    create_before_destroy = true
+  }    
 }
 
 resource "aws_security_group" "network" {
@@ -38,10 +43,17 @@ resource "aws_autoscaling_group" "auto-scale" {
   desired_capacity   = 2
   max_size           = 4
   min_size           = 2
+  vpc_zone_identifier = data.aws_subnets.default.ids
 
   launch_template {
     id      =  aws_launch_template.main_server.id
     version = "$Latest"
+  }
+  
+  tag {
+    key = "Name"
+    value = "terraform-server"
+    propagate_at_launch = true
   }
 }
 
@@ -54,4 +66,15 @@ variable "server_port" {
 output "ec2-public-address" {
   value = aws_instance.main_server.public_ip
   description = "The public IP address of theweb server."
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name = "vpc-id"
+    values = [ data.aws_vpc.default.id ]
+  }
 }
