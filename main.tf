8b63732df39933c1b911e0d2ec19a6f1697d19a2
Samuel Ajala <samuelaj220@gmail.com>
2026-07-10 21:10:54 +0100

    feat: configure application load balancer

--- main.tf
+++ main.tf
@@ -49,11 +49,96 @@ resource "aws_autoscaling_group" "auto-scale" {
     id      =  aws_launch_template.main_server.id
     version = "$Latest"
   }
+
+  # Links the ASG dynamically to the ALB target group
+  target_group_arns = [ aws_lb_target_group.asg.arn ]
+  
+  # Changes health check tracking from standard EC2 to the Load Balancer
+  health_check_type = "ELB"
+  health_check_grace_period = 300
   
   tag {
     key = "Name"
     value = "terraform-server"
     propagate_at_launch = true
+  }
+}
+
+resource "aws_lb" "load-balancer" {
+  name = "terraform-load"
+  load_balancer_type = "application"
+  subnets = data.aws_subnets.default.ids
+  security_groups = [ aws_security_group.alb.id ]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.load-balancer.arn
+  port = 80
+  protocol = "HTTP"
+
+  # By default return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code = 404
+    }
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "alb-security-group"
+
+  # Allow inbound HTTP requests
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+   # Allow all outbound requests
+   egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+}
+
+resource "aws_lb_target_group" "asg" {
+  name = "terraform-target-group"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "204"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "listener-rule" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+
+  condition {
+    path_pattern {
+      values = [ "*" ]
+    }
+  }
+
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
   }
 }
 
@@ -63,9 +148,9 @@ variable "server_port" {
   default = 8080
 }
 
-output "ec2-public-address" {
-  value = aws_instance.main_server.public_ip
-  description = "The public IP address of theweb server."
+output "alb-dns-name" {
+  value = aws_lb.load-balancer.dns_name
+  description = "The domain name of the load balancer."
 }
 
 data "aws_vpc" "default" {
