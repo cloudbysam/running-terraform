bf476beb1df778641769d3f4a3800ab7f0049355
Samuel Ajala <samuelaj220@gmail.com>
2026-09-08 13:06:37 +0100

    feat: Import existing staging security and target groups

--- envs/stage/services/webserver-cluster/main.tf
+++ envs/stage/services/webserver-cluster/main.tf
@@ -42,6 +42,21 @@ output "alb_dns_name" {
   description = "The domain name of the load balancer"
 }
 
+import {
+  to = module.webserver-cluster.aws_security_group.network
+  id = "sg-0d93e40d345240598" 
+}
+
+import {
+  to = module.webserver-cluster.aws_security_group.alb
+  id = "sg-0e20cb37140abe757" 
+}
+
+import {
+  to = module.webserver-cluster.aws_lb_target_group.asg
+  id = "arn:aws:elasticloadbalancing:us-east-1:662829279164:targetgroup/stage-target-group/5d965c912ca0aac0" 
+}
+
 # Partial configuration: remaining settings (e.g., bucket, region)
 # must be passed via '-backend-config' arguments during 'terraform init'. 
 # terraform init -backend-config="backend.hcl"
