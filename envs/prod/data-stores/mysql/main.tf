637070dc52244c6f8699ab690cf1abc39b29e337
Samuel Ajala <samuelaj220@gmail.com>
2026-09-10 11:58:12 +0100

    fix(db): swap underscores to hyphens for AWS identifier compatibility

--- envs/prod/data-stores/mysql/main.tf
+++ envs/prod/data-stores/mysql/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "mysql_database" {
   source = "../../../../modules/data-stores/mysql"
-  db_name = "aws_database_stage"
+  db_name = "aws-database-prod"
   instance_class    = "db.t3.micro"
   allocated_storage = 10
 
