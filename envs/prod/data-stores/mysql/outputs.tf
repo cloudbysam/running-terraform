6c0d897339b768fb2c24cb29cdd9939e904cde93
Samuel Ajala <samuelaj220@gmail.com>
2026-09-10 11:48:36 +0100

    fix(outputs): update env outputs to read from the mysql module

--- envs/prod/data-stores/mysql/outputs.tf
+++ envs/prod/data-stores/mysql/outputs.tf
@@ -1,9 +1,9 @@
 output "address" {
-  value       = aws_db_instance.database.address
+  value       = module.mysql_database.address
   description = "connect to the database at this endpoint."
 }
 
 output "port" {
-  value       = aws_db_instance.database.port
+  value       = module.mysql_database.port
   description = "The port the database is listening on."
 }
